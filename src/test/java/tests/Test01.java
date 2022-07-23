48ade93a6b5625cd19a578cd07c8295e63ee21f9
Recep Aykurt <recep.aykurt7@gmail.com>
2022-07-24 00:26:05 +0300

    Second

--- src/test/java/tests/Test01.java
+++ src/test/java/tests/Test01.java
@@ -4,11 +4,11 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import pages.*;
 import utilities.Driver;
+import utilities.ExcelUtil;
+import utilities.Log;
 
 import java.time.Duration;
 import java.util.ArrayList;
@@ -22,6 +22,7 @@ public class Test01 {
     Urunler urunlerObj = new Urunler();
     Listelerim listelerimObj = new Listelerim();
     Sepetim sepetimobj = new Sepetim();
+    ExcelUtil excelUtil;
     WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(20));
     Random randomObj=new Random();
     String secilenUrunBasligi;
@@ -29,23 +30,35 @@ public class Test01 {
 
 
 
-    @BeforeMethod
+    @BeforeSuite
     public void setUp () {
         Driver.getDriver().navigate().to("https://www.amazon.com.tr/");
     }
 
-    @Test
-    public void Test1 () {
 
+    @Test
+    public void Test01 () {
+
+        Log.warn("Test Basliyor...");
         wait.until(ExpectedConditions.elementToBeClickable(anasayfaObj.hesapVeListelerButonu));
         anasayfaObj.hesapVeListelerButonu.click();
 
+    }
+
+    @Test(dataProvider = "loginInfo")
+    public void Test02_UserLogin(String username, String password) {
         Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        girisYapObj.epostaTextKutusu.sendKeys("raykurt@hotmail.com" + Keys.ENTER);
+        Log.info("Uyelik e-posta bilgisi giriliyor...");
+        girisYapObj.epostaTextKutusu.sendKeys(username + Keys.ENTER);
 
         Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        girisYapObj.sifreTextKutusu.sendKeys("123Recep");
+        Log.info("Uyelik sifre bilgisi giriliyor...");
+        girisYapObj.sifreTextKutusu.sendKeys(password);
         girisYapObj.girisYapButonu.click();
+    }
+
+    @Test
+    public void Test03 () {
 
         Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         anasayfaObj.searchTextKutusu.sendKeys("samsung");
@@ -62,10 +75,12 @@ public class Test01 {
 
         Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("sr_pg_2"));
 
+        Log.info("Ustten 5. Urun Inceleniyor...");
         Driver.waitAndClick(urunlerObj.usttenBesinciUrun);
 
         secilenUrunBasligi = urunlerObj.secilenUrunBasligi.getText();
 
+        Log.info("Urun Listeye Ekleniyor...");
         Driver.waitAndClick(urunlerObj.urunuListeyeEkleButonu);
 
         Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -78,6 +93,7 @@ public class Test01 {
         }
         Assert.assertTrue(listOfString.contains(secilenUrunBasligi));
 
+        Log.info("Listedeki Urun Sepete Ekleniyor...");
         Driver.waitAndClick(listelerimObj.listedekiUrunuSepeteEkleButonu);
 
         Driver.wait(2);
@@ -87,15 +103,26 @@ public class Test01 {
 
         Assert.assertEquals(sepetimobj.sepettekiUrunBasligi.getText(), secilenUrunBasligi);
 
+        Log.info("Sepetteki Urun Siliniyor...");
         Driver.waitAndClick(sepetimobj.sepettekiUrunuSilButonu);
 
         Assert.assertTrue(sepetimobj.sepettekiUrunuSilAssert.getText().contains("Alışveriş Sepetiniz konumundan kaldırıldı."));
 
     }
 
-    @AfterMethod
+    @DataProvider(name = "loginInfo")
+    public Object [][] getData(){
+        String path = "./src/test/java/resources/optiim.xlsx";
+        String sheetName="login_info";
+        excelUtil = new ExcelUtil(path,sheetName);
+        Object [][]  loginCredentials = excelUtil.getDataArrayWithoutFirstRow();
+        return loginCredentials;
+    }
+
+    @AfterSuite
     public void tearDown () {
-        System.out.println("This is after method");
+        Log.warn("Test Bitti... Driver Kapatiliyor...");
+        Driver.closeDriver();
     }
 
 }
