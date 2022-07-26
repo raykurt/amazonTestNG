fa894e47722ce1f3ea2ef5ec5dd761a3b5eed134
Recep Aykurt <recep.aykurt7@gmail.com>
2022-07-26 23:09:36 +0300

    Third

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -4,6 +4,10 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.io.File;
 import java.io.IOException;
@@ -22,8 +26,23 @@ public class Driver {
 
     public static WebDriver getDriver() {
         if (driver == null) {
+            String browser = ConfigurationReader.getProperty("browser");
+            if ("chrome".equals(browser)) {
                 WebDriverManager.chromedriver().setup();
                 driver = new ChromeDriver();
+            } else if ("firefox".equals(browser)) {
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+            } else if ("ie".equals(browser)) {
+                WebDriverManager.iedriver().setup();
+                driver = new InternetExplorerDriver();
+            } else if ("safari".equals(browser)) {
+                WebDriverManager.getInstance(SafariDriver.class).setup();
+                driver = new SafariDriver();
+            } else if ("chrome-headless".equals(browser)) {
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
+            }
         }
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
